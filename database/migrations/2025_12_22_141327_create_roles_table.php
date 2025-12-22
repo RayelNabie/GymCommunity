4387a3c4b799d846151fa9ab659599b887dff246
Rayel <rayelnabie@gmail.com>
2025-12-22 15:25:03 +0100

    fix: Change id to role_id for consistency to model

--- database/migrations/2025_12_22_141327_create_roles_table.php
+++ database/migrations/2025_12_22_141327_create_roles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('role_id')->primary();
             $table->string('name')->unique();
             $table->string('description')->nullable();
             $table->timestamps();

0e73c6f22ee2e30ee7f84120132a08a197766540
Rayel <rayelnabie@gmail.com>
2025-12-22 15:23:36 +0100

    feat: Add roles model, no foreignkeys yet

---
+++ app/Models/Role.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasUuids;
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'role_id';
+
+    /**
+     * Disable auto-incrementing since UUID's dont auto increment
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * Turn PK into string since UUID's aren't ints like normal id's
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}

c0a13de0e884852636f68c93864f1506f50c74f4
Rayel <rayelnabie@gmail.com>
2025-12-29 13:59:42 +0100

    feat: Link controller to existing view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,7 +12,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::with('user')->latest()->get();
+        return view('artikelen.index', compact('posts'), [
+            'posts' => $posts
+        ]);
     }
 
     /**

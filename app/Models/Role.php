8510eaed686383355c41f5b3050b169a09a2f211
Rayel <rayelnabie@gmail.com>
2026-01-04 17:35:17 +0100

    feat: Add new adminscopes to model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @property string $name
+ */
 class Role extends Model
 {
     use HasUuids;
